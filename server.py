e4b9ed5a3a9c94044c211280c9ec2d25375993ec
sakshi <sakshidaware2002@gmail.com>
2024-12-26 14:59:25 +0530

    2nd commit

--- server.py
+++ server.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def root():
-    return "flask application version 1.0"
+    return "flask application version 1.1"
 
 app.run(host="0.0.0.0", port=4000)
